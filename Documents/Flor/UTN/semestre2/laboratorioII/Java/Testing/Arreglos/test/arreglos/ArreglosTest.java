827a1fa6f7ec8421437ac8263296838ff36a5222
Aaron-CR <46272859+Aaron-CR@users.noreply.github.com>
2019-04-30 10:44:23 -0300

    Add files via upload

---
+++ Documents/Flor/UTN/semestre2/laboratorioII/Java/Testing/Arreglos/test/arreglos/ArreglosTest.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Alumno
+ */
+public class ArreglosTest {
+    Arreglos instance = new Arreglos();
+    
+    public ArreglosTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        instance = new Arreglos();
+    }
+    
+    @After
+    public void tearDown() {
+        instance = null;
+    }
+
+    /**
+     * Test of sumaArreglos method, of class Arreglos.
+     */
+    @Test
+    public void testSumaArreglos() {
+        System.out.println("sumaArreglos");
+        int[] a = {1, 1, 1};
+        int[] b = {1, 1, 1};
+        int[] expResult = {2, 2, 2};
+        int[] result = instance.sumaArreglos(a, b);
+        assertArrayEquals(expResult, result);
+    }
+
+    /**
+     * Test of menorDelArreglo method, of class Arreglos.
+     */
+    @Test
+    public void testMenorDelArreglo() {
+        System.out.println("menorDelArreglo");
+        int[] a = {1, 2, 3};
+        int expResult = 1;
+        int result = instance.menorDelArreglo(a);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of sumaSoloPares method, of class Arreglos.
+     */
+    @Test
+    public void testSumaSoloPares() {
+        System.out.println("sumaSoloPares");
+        int[] a = {1, 2, 2};
+        int expResult = 4;
+        int result = instance.sumaSoloPares(a);
+        assertEquals(expResult, result);
+    }
+    
+}
