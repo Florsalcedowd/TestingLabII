4ed94c3913b027919ed2d32f1ad54162b0bb5d6d
Florsalcedowd <florenciasalcedo13@gmail.com>
2019-05-06 19:06:46 -0300

    Código generado para CalculoAvanzado

--- Calculadora/src/calculadora/CalculoAvanzado.java
+++ Calculadora/src/calculadora/CalculoAvanzado.java
@@ -5,10 +5,31 @@
  */
 package calculadora;
 
+import static java.lang.Math.cbrt;
+import static java.lang.Math.sqrt;
+
 /**
  *
  * @author flocy
  */
 public class CalculoAvanzado {
+    float raizCuadrada(float a){
+        float rCuad= (float) sqrt(a);
+        return rCuad;
+    } 
     
+    float raizCubica(float a){
+        float rCub= (float) cbrt(a);
+        return rCub;
+    }
+    
+    float cuadrado(float a){
+        float cuad= (float) Math.pow(a,2);
+        return cuad;
+    }
+    
+    float inversoMultiplicativo(float a){
+        float inv= 1/a;
+        return inv;
+    }
 }
