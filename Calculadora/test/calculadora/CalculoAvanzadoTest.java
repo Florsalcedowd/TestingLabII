509ed4ba66871ebca56691d7d6af07172bbc4fee
Aaron-CR <46272859+Aaron-CR@users.noreply.github.com>
2019-05-06 18:42:14 -0300

    Test agregados

--- Calculadora/test/calculadora/CalculoAvanzadoTest.java
+++ Calculadora/test/calculadora/CalculoAvanzadoTest.java
@@ -17,10 +17,56 @@ public class CalculoAvanzadoTest {
     public CalculoAvanzadoTest() {
     }
 
+    /**
+     * Test of suma method, of class CalculoAvanzado.
+     */
     @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testRaizCuadrada() {
+        System.out.println("raíz cuadrada");
+        float a = 9.0F;
+        CalculoAvanzado instance = new CalculoAvanzado();
+        float expResult = 3.0F;
+        float result = instance.raizCuadrada(a);
+        assertEquals(expResult, result, 0.0);
+    }
+    
+    /**
+     * Test of suma method, of class CalculoAvanzado.
+     */
+    @Test
+    public void testRaizCubica() {
+        System.out.println("raíz cubica");
+        float a = 8.0F;
+        CalculoAvanzado instance = new CalculoAvanzado();
+        float expResult = 2.0F;
+        float result = instance.raizCubica(a);
+        assertEquals(expResult, result, 0.0);
+    }
+    
+    /**
+     * Test of suma method, of class CalculoAvanzado.
+     */
+    @Test
+    public void testCuadrado() {
+        System.out.println("cuadrado");
+        float a = 3.0F;
+        CalculoAvanzado instance = new CalculoAvanzado();
+        float expResult = 9.0F;
+        float result = instance.cuadrado(a);
+        assertEquals(expResult, result, 0.0);
+    }
+    
+    /**
+     * Test of suma method, of class CalculoAvanzado.
+     */
+    @Test
+    public void testInversoMultiplicativo() {
+        System.out.println("inverso multiplicativo");
+        float a = 8.0F;
+        CalculoAvanzado instance = new CalculoAvanzado();
+        float expResult = 0.125F;
+        float result = instance.inversoMultiplicativo(a);
+        assertEquals(expResult, result, 0.0);
     }
     
 }
--- Calculadora/test/calculadora/CalculoSimpleTest.java
+++ Calculadora/test/calculadora/CalculoSimpleTest.java
@@ -16,11 +16,61 @@ public class CalculoSimpleTest {
     
     public CalculoSimpleTest() {
     }
-
+    
+    /**
+     * Test of suma method, of class CalculoSimple.
+     */
     @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testSuma() {
+        System.out.println("suma");
+        int a = 5;
+        int b = 5;
+        CalculoSimple instance = new CalculoSimple();
+        int expResult = 10;
+        int result = instance.suma(a, b);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of resta method, of class CalculoSimple.
+     */
+    @Test
+    public void testResta() {
+        System.out.println("resta");
+        int a = 10;
+        int b = 5;
+        CalculoSimple instance = new CalculoSimple();
+        int expResult = 5;
+        int result = instance.resta(a, b);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of division method, of class CalculoSimple.
+     */
+    @Test
+    public void testDivision() {
+        System.out.println("division");
+        float a = 9.0F;
+        float b = 2.0F;
+        CalculoSimple instance = new CalculoSimple();
+        float expResult = 4.5F;
+        float result = instance.division(a, b);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of multiplicacion method, of class CalculoSimple.
+     */
+    @Test
+    public void testMultiplicacion() {
+        System.out.println("multiplicacion");
+        int a = 4;
+        int b = 9;
+        CalculoSimple instance = new CalculoSimple();
+        int expResult = 36;
+        int result = instance.multiplicacion(a, b);
+        assertEquals(expResult, result);
     }
     
 }
