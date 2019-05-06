2655214e03a0f9858af945ead6b8c2639106569d
Florsalcedowd <florenciasalcedo13@gmail.com>
2019-05-06 19:06:59 -0300

    Código generado para CalculoSimple

--- Calculadora/src/calculadora/CalculoSimple.java
+++ Calculadora/src/calculadora/CalculoSimple.java
@@ -10,5 +10,25 @@ package calculadora;
  * @author flocy
  */
 public class CalculoSimple {
+
+    int suma(int a, int b) {
+        int sum= a + b;
+        return sum;
+    }
+
+    int resta(int a, int b) {
+        int res= a-b;
+        return res;
+    }
+
+    float division(float a, float b) {
+        float div= a/b;
+        return div;
+    }
+
+    int multiplicacion(int a, int b) {
+        int mul= a*b;
+        return mul;
+    }
     
 }
